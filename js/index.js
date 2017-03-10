70e43730959ce4d84bec7977cd16675a89d24574
Tim <haytowntim@embarqmail.com>
2017-03-09 20:49:09 -0500

    Better gearliftclaw graphics and auto selector

--- js/index.js
+++ js/index.js
@@ -2,11 +2,32 @@
 var connectedstate = document.getElementById("connectedstate");
 var robotstate = document.getElementById("robotstate");
 var robotaddress = document.getElementById("robotAddress");
+
 var header = document.getElementById("header");
-var cameracell = document.getElementById("camera-cell");
+var debugbtn = document.getElementById("debug-btn");
+var notdebugbtn = document.getElementById("not-debug-btn");
+
+var cameradiv = document.getElementById("camera-div");
+var debugdiv = document.getElementById("debug-div");
+var notdebugdiv = document.getElementById("not-debug-div");
+
+var autoselect = document.getElementById("auto-select");
+
 var cameravalue = document.getElementById("camera-value");
 var processvalue = document.getElementById("process-value");
 var turnvalue = document.getElementById("turn-value");
+
+var clawdiv = document.getElementById("claw-div");
+var clawstate = document.getElementById("claw-state");
+
+var gearliftstate = document.getElementById("gearliftstate");
+var liftup = document.getElementById("lift-up");
+var liftdown = document.getElementById("lift-down");
+var gearopen = document.getElementById("gear-open");
+var gearclose = document.getElementById("gear-close");
+var gearstate = document.getElementById("gear-state");
+var gearimg = document.getElementById("gear-img");
+
 var lower_red = document.getElementById("lower_red");
 var upper_red = document.getElementById("upper_red");
 var lower_blue = document.getElementById("lower_blue");
@@ -14,22 +35,77 @@ var upper_blue = document.getElementById("upper_blue");
 var lower_green = document.getElementById("lower_green");
 var upper_green = document.getElementById("upper_green");
 
-//$("#red-slider").slider({max: 255, min: 0, range: true});
-//$("#blue-slider").slider({max: 255, min: 0, range: true});
-//$("#green-slider").slider({max: 255, min: 0, range: true});
 
 console.log("Starting");
+
+debugdiv.style.display = "none";
+notdebugdiv.style.display = "inline-block";
+
+debugbtn.addEventListener("click", function(e){
+  debugdiv.style.display = "inline-block";
+  notdebugdiv.style.display = "none";
+});
+
+notdebugbtn.addEventListener("click", function(e){
+  debugdiv.style.display = "none";
+  notdebugdiv.style.display = "inline-block";
+});
+
+  
 // sets a function that will be called when the websocket connects/disconnects
 NetworkTables.addWsConnectionListener(onNetworkTablesConnection, true);
-
 // sets a function that will be called when the robot connects/disconnects
 NetworkTables.addRobotConnectionListener(onRobotConnection, true);
-
 // sets a function that will be called when any NetworkTables key/value changes
 NetworkTables.addGlobalListener(onValueChanged, true);
 
+
+
+var smoothie = new SmoothieChart({
+  minValue: 0,
+  maxValue: 10
+});
+smoothie.streamTo(document.getElementById("current"));
+
+var masterliftcurrentline = new TimeSeries({
+  strokeStyle:'#0000ff'
+});
+
+var slaveliftcurrentline = new TimeSeries({
+  strokeStyle:'#ff0000'
+});
+
+var clawcurrentline = new TimeSeries({
+  strokeStyle:'#00ff00'
+});
+
+
+smoothie.addTimeSeries(masterliftcurrentline);
+smoothie.addTimeSeries(slaveliftcurrentline);
+smoothie.addTimeSeries(clawcurrentline);
+
+NetworkTables.addKeyListener("/Robot/masterliftcurrent", function(key, value, isNew){
+  masterliftcurrentline.append(new Date().getTime(), value);
+}, true);
+
+NetworkTables.addKeyListener("/Robot/slaveliftcurrent", function(key, value, isNew){
+  slaveliftcurrentline.append(new Date().getTime(), value);
+}, true);
+
+NetworkTables.addKeyListener("/Robot/clawcurrent", function(key, value, isNew){
+  clawcurrentline.append(new Date().getTime(), value);
+}, true);
+
+setInterval(function(){
+  masterliftcurrentline.append(new Date().getTime(), NetworkTables.getValue("/Robot/masterliftcurrent"));
+  slaveliftcurrentline.append(new Date().getTime(), NetworkTables.getValue("/Robot/slaveliftcurrent"));
+  clawcurrentline.append(new Date().getTime(), NetworkTables.getValue("/Robot/clawcurrent"));
+}, 500);
+
+
+
 NetworkTables.addKeyListener("/RaspberryPi/lock", function(key, value, isNew){
-  cameracell.style.backgroundColor = value ? 'green' : 'white';
+  cameradiv.style.backgroundColor = value ? 'green' : 'white';
 }, true);
 
 NetworkTables.addKeyListener("/RaspberryPi/camera", function(key, value, isNew){
@@ -48,6 +124,71 @@ NetworkTables.addKeyListener("/RaspberryPi/turn", function(key, value, isNew){
 NetworkTables.addKeyListener("/RaspberryPi/turn", function(key, value, isNew){
   turnvalue.innerHTML = parseFloat(value, 10).toFixed(5);
 }, true);
+
+
+/*
+NetworkTables.addKeyListener("/Robot/hasgear", function(key, value, isNew){
+  clawdiv.style.backgroundColor = value ? 'green' : 'white';
+}, true);
+
+NetworkTables.addKeyListener("/Robot/clawstate", function(key, value, isNew){
+  clawstate.innerHTML = value;
+}, true);
+*/
+
+
+NetworkTables.putValue("/Robot/automode", autoselect.value);
+
+autoselect.addEventListener("change", function(e){
+  NetworkTables.putValue("/Robot/automode", autoselect.value);
+});
+
+
+
+NetworkTables.addKeyListener("/Robot/liftup", function(key, value, isNew){
+  liftup.style.backgroundColor = value ? "green" : "grey";
+}, true);
+
+
+NetworkTables.addKeyListener("/Robot/liftdown", function(key, value, isNew){
+  liftdown.style.backgroundColor = value ? "green" : "grey";
+}, true);
+
+
+NetworkTables.addKeyListener("/Robot/clawstate", function(key, value, isNew){
+  if(value === "OPEN"){
+    gearopen.style.backgroundColor = "green";
+    gearclose.style.backgroundColor = "grey";
+  }else if(value === "OPENING"){
+    gearopen.style.backgroundColor = "yellow";
+    gearclose.style.backgroundColor = "grey";
+  }else if(value === "CLOSING"){
+    gearopen.style.backgroundColor = "grey";
+    gearclose.style.backgroundColor = "yellow";
+  }else if(value === "GRABBING"){
+    gearopen.style.backgroundColor = "grey";
+    gearclose.style.backgroundColor = "green";
+  }else if(value === "MISSED"){
+    gearopen.style.backgroundColor = "grey";
+    gearclose.style.backgroundColor = "grey";
+  }else if(value === "STOPPED_OPENING"){
+    gearopen.style.backgroundColor = "red";
+    gearclose.style.backgroundColor = "grey";
+  }else if(value === "STOPPED_CLOSING"){
+    gearopen.style.backgroundColor = "grey";
+    gearclose.style.backgroundColor = "red";
+  }
+}, true);
+
+NetworkTables.addKeyListener("/Robot/gearswitch", function(key, value, isNew){
+  gearstate.style.backgroundColor = value ? "green" : "grey";
+}, true);
+
+NetworkTables.addKeyListener("/Robot/hasgear", function(key, value, isNew){
+  gearimg.style.display = value ? "block" : "none";
+}, true);
+
+
 
 NetworkTables.addKeyListener("/RaspberryPi/lower_red", function(key, value, isNew){
   lower_red.value = value;
@@ -124,7 +265,7 @@ function onValueChanged(key, value, isNew) {
 	// the id of the elements that we're appending, for simplicity. However,
 	// the key names aren't always valid HTML identifiers, so we use
 	// the NetworkTables.keyToId() function to convert them appropriately
-	if (isNew && (key.startsWith("/RaspberryPi") || key.startsWith("/SmartDashboard"))) {
+	if (isNew && (key.startsWith("/RaspberryPi") || key.startsWith("/SmartDashboard") || key.startsWith("/Robot"))) {
 		var tr = $('<tr></tr>').appendTo($('#nt > tbody:last'));
 		$('<td></td>').text(key).appendTo(tr);
 		$('<td></td>').attr('id', NetworkTables.keyToId(key))
